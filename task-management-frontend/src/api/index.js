888a0fc4412a12c473e3b684acb0d814a2825d09
TuDinhVuu <tuvdhe172765@fpt.edu.vn>
2024-09-25 17:15:32 +0700

    next

--- task-management-frontend/src/api/index.js
+++ task-management-frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://localhost:5042/api', // Update this with your API URL
+  baseURL: 'https://localhost:5042/api', 
   headers: {
     'Content-Type': 'application/json',
   },
